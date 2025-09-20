dff3d9f0759110ee424091fe3f975f53668fbd9e
judivinko <judi.vinko81@gmail.com>
2025-09-20 15:56:47 +0200

    server.js

--- server.js
+++ server.js
@@ -818,6 +818,7 @@ res.json({ ok:true, ...result });
   }
   return res.status(400).json({ ok:false, error: String(e.message || e) });
 }
+});
 
 
 
@@ -1240,6 +1241,7 @@ server.listen(PORT, HOST, () => {
   console.log(`ARTEFACT server listening on http://${HOST}:${PORT}`);
 });
         //---end
+
 
 
 
